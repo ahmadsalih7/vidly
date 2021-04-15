4652cc1ec80c26b851a4b1cb47f3d1d2dc0fadee
Ahmed Salih <ahmadsalih92@gmail.com>
2021-04-16 02:37:55 +0300

    Import express and JOI and preparing listening port

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const Joi = require('joi');
+
+var genres = [
+    {id: 1, genre: "Horror"},
+    {id: 2, genre: "Action"},
+    {id: 3, genre: "Comdey"},
+];
+
+app.listen(3000, ()=>{
+    console.log("Listening to port 3000 .... ");
+});
