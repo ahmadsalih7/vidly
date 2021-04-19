fedf89ef117922b435e7f3a4b481cda85b22d08a
Ahmed Salih <ahmadsalih92@gmail.com>
2021-04-20 01:53:39 +0300

    Refactor!: move genres route handling to genres.js
    Using route object instead of app object to handle /api/genres
    and move it to a sperate file routes/genres.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const logger = require('./logger');
 const startupDebugger = require('debug')('app:startup');
 const dbdebugger = require('debug')('app:db');
 const app = express();
-const Joi = require('joi');
+const genres = require('./routes/genres');
 
+app.use('/api/genres', genres);
 app.set('view engine', 'pug');
 app.use(express.json());
 app.use(logger);
@@ -17,12 +18,6 @@ console.log(`password: ${config.get('mail.password')}`);
 startupDebugger("This is a startup message");
 dbdebugger("This is a db message");
 
-var genres = [
-    {id: 1, name: "Horror"},
-    {id: 2, name: "Action"},
-    {id: 3, name: "Comdey"},
-];
-
 // preparing listening ports
 app.listen(3000, ()=>{
     console.log("Listening to port 3000.... ");
@@ -32,79 +27,3 @@ app.listen(3000, ()=>{
 app.get('/', (req, res) => {
     res.render('index', {title: "Vidly", message: "Welcome to Vidly"});
 });
-
-// Get genres response
-app.get('/api/genres', (req, res)=>{
-    res.send(genres);
-});
-
-// Get genre by ID
-app.get('/api/genres/:id', (req, res) =>{
-    // Check if the ID is existed
-    const genre = genres.find(x=> x.id === parseInt(req.params.id));
-    // if there is no genre found 
-    if (!genre) return res.status(404).send("No genre was found with this ID.");
-    // else: 
-    res.send(genre);
-});
-
-// Add genre using POST 
-app.post('/api/genres', (req, res)=>{
-    //validate the JSON input is a valid genre object
-    const {error} = validateGenre (req.body);
-    // If it's  not valid send the error and return
-    if (error) return res.status(400).send(error.details[0].message);
-    // else create genre object
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name
-    };
-    // append it to the current genres 
-    genres.push(genre);
-    // response with the appended genre
-    res.send(genre);
-});
-
-// Update genre using PUT request
-
-app.put('/api/genres/:id', (req, res) => {
-    // Check if the ID is existed
-    const genre = genres.find(x=> x.id === parseInt(req.params.id));
-    // if there is no genre found
-    if (!genre) return res.status(404).send("No genre was found with this ID.");
-    //validate the JSON input is a valid genre object
-    const {error} = validateGenre (req.body);
-    // If it's  not valid send the error and return
-    if (error) return res.status(400).send(error.details[0].message);
-    // else update genre object
-    genre.name = req.body.name;
-    // response with the appended genre
-    res.send(genre);
-});
-
-// delete genres using delete request
-app.delete('/api/genres/:id', (req, res)=> {
-// Check if the ID is existed
-const genre = genres.find(x=> x.id === parseInt(req.params.id));
-// if there is no genre found
-if (!genre) return res.status(404).send("No genre was found with this ID.");
-// else delete genre object
-const index = genres.indexOf(genre);
-genres.splice(index, 1);
-
-//Response with the deleted genre
-res.send(genre);
-
-});
-
-
-
-// add validate function
-function validateGenre(genre) {
-    //Define schema 
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    
-    return schema.validate(genre);
-}
---
+++ routes/genres.js
@@ -0,0 +1,89 @@
+//import router object
+const express = require('express');
+router = express.Router();
+
+var genres = [
+    {id: 1, name: "Horror"},
+    {id: 2, name: "Action"},
+    {id: 3, name: "Comdey"},
+];
+
+// Get genres response
+router.get('/', (req, res)=>{
+    res.send(genres);
+});
+
+// Get genre by ID
+router.get('/:id', (req, res) =>{
+    // Check if the ID is existed
+    const genre = genres.find(x=> x.id === parseInt(req.params.id));
+    // if there is no genre found 
+    if (!genre) return res.status(404).send("No genre was found with this ID.");
+    // else: 
+    res.send(genre);
+});
+
+// Add genre using POST 
+router.post('/', (req, res)=>{
+    //validate the JSON input is a valid genre object
+    const {error} = validateGenre (req.body);
+    // If it's  not valid send the error and return
+    if (error) return res.status(400).send(error.details[0].message);
+    // else create genre object
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    // append it to the current genres 
+    genres.push(genre);
+    // response with the appended genre
+    res.send(genre);
+});
+
+// Update genre using PUT request
+
+router.put('/:id', (req, res) => {
+    // Check if the ID is existed
+    const genre = genres.find(x=> x.id === parseInt(req.params.id));
+    // if there is no genre found
+    if (!genre) return res.status(404).send("No genre was found with this ID.");
+    //validate the JSON input is a valid genre object
+    const {error} = validateGenre (req.body);
+    // If it's  not valid send the error and return
+    if (error) return res.status(400).send(error.details[0].message);
+    // else update genre object
+    genre.name = req.body.name;
+    // response with the appended genre
+    res.send(genre);
+});
+
+// delete genres using delete request
+router.delete('/:id', (req, res)=> {
+// Check if the ID is existed
+const genre = genres.find(x=> x.id === parseInt(req.params.id));
+// if there is no genre found
+if (!genre) return res.status(404).send("No genre was found with this ID.");
+// else delete genre object
+const index = genres.indexOf(genre);
+genres.splice(index, 1);
+
+//Response with the deleted genre
+res.send(genre);
+
+});
+
+
+
+// add validate function
+function validateGenre(genre) {
+    //Define schema 
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    
+    return schema.validate(genre);
+}
+
+//export the Router obejct
+
+module.exports = router;
